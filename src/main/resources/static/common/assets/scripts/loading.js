87866241ac0f28b44a5304dca6ea498325726d46
kwRyu.dev <rkw2115@gmail.com>
2025-07-10 11:03:53 +0900

    [류경완, 로딩 동작관련 기능 구현] loading ui 구현 loading ON/OFF 기능 구현 loading gif 파일 추가

---
+++ src/main/resources/static/common/assets/scripts/loading.js
@@ -0,0 +1,45 @@
+export class Loading {
+    #$element;
+    #$caption;
+
+    constructor() {
+        this.#$element = document.createElement('div');
+        this.#$element.className = '-object-loading';
+        this.#$element.setVisible(false); // 초기 숨김
+
+        const $icon = document.createElement('img');
+        $icon.className = 'icon';
+        $icon.src = '/test/assets/loading.gif';
+
+        this.#$caption = document.createElement('div');
+        this.#$caption.className = 'caption';
+        this.#$caption.textContent = '잠시만 기다려주세요.';
+
+        this.#$element.append($icon, this.#$caption);
+        document.body.appendChild(this.#$element);
+    }
+
+    /**
+     * 로딩 표시
+     * @param {string} message
+     */
+    show(message = '잠시만 기다려주세요.') {
+        this.#$caption.textContent = message;
+        this.#$element.setVisible(true);
+    }
+
+    /**
+     * 로딩 숨김
+     */
+    hide() {
+        this.#$element.setVisible(false);
+    }
+
+    /**
+     * 로딩 표시 여부
+     * @returns {boolean}
+     */
+    isVisible() {
+        return this.#$element.isVisible();
+    }
+}
