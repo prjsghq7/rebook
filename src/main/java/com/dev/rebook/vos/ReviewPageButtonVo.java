bf59c3b8d4f4dd1764d3939384b4fff020dd5ca4
ImDongHo <imtongho1@gmail.com>
2025-07-28 13:53:07 +0900

    [임동호, 리뷰화면 내 키워드 검색 추가] 리뷰화면 내 키워드 검색 코드 추가 및 쿼리문 수정 검색 이미지 추가

--- src/main/java/com/dev/rebook/vos/ReviewPageButtonVo.java
+++ src/main/java/com/dev/rebook/vos/ReviewPageButtonVo.java
@@ -10,8 +10,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ReviewPageButtonVo {
+    private String keyword;
     private int sortType = 0;
-    private String sort = "DESC";
+    private Boolean sort = false;
     private Boolean adult = false;
     private Boolean mine = false;
 }
--- src/main/resources/static/review/assets/scripts/index.js
+++ src/main/resources/static/review/assets/scripts/index.js
@@ -1,32 +1,39 @@
 const $buttonForm = document.getElementById('button-form');
+const $searchInput = $buttonForm.querySelector(`:scope > .button-container > .search > .button-label > .button-input`);
 
-$buttonForm['sortType'].forEach(function ($button) {
-    $button.addEventListener('change', function () {
+$buttonForm['sortType'].forEach(($button) => {
+    $button.addEventListener('change', () => {
         if ($buttonForm['mine'] === undefined) {
-            location.href = `${window.origin}/review/?sortType=${$button.value}`;
+            location.href = `${window.origin}/review/?sortType=${$button.value}&sort=${$buttonForm['sort'].checked}&keyword=${$searchInput.value}`;
         } else {
-            location.href = `${window.origin}/review/?sortType=${$button.value}&mine=${$buttonForm['mine'].checked}`;
+            location.href = `${window.origin}/review/?sortType=${$button.value}&mine=${$buttonForm['mine'].checked}&sort=${$buttonForm['sort'].checked}&keyword=${$searchInput.value}`;
         }
     });
 });
 
+
 if ($buttonForm['mine'] !== undefined) {
-    $buttonForm['mine'].addEventListener('change', function () {
-        location.href = `${window.origin}/review/?sortType=${$buttonForm['sortType'].value}&mine=${$buttonForm['mine'].checked}`;
+    $buttonForm['mine'].addEventListener('change', () => {
+        location.href = `${window.origin}/review/?sortType=${$buttonForm['sortType'].value}&mine=${$buttonForm['mine'].checked}&sort=${$buttonForm['sort'].checked}&keyword=${$searchInput.value}`;
     });
 }
 
-$buttonForm['sort'].addEventListener('change', function () {
-    let sortText = "";
-    if ($buttonForm['sort'].checked) {
-        sortText = "ASC";
-    } else {
-        sortText = "DESC";
-    }
-
+$buttonForm['sort'].addEventListener('change', () => {
     if ($buttonForm['mine'] !== undefined) {
-        location.href = `${window.origin}/review/?sortType=${$buttonForm['sortType'].value}&mine=${$buttonForm['mine'].checked}&sort=${sortText}`;
-        } else {
-        location.href = `${window.origin}/review/?sortType=${$buttonForm['sortType'].value}&sort=${sortText}`;
+        location.href = `${window.origin}/review/?sortType=${$buttonForm['sortType'].value}&mine=${$buttonForm['mine'].checked}&sort=${$buttonForm['sort'].checked}&keyword=${$searchInput.value}`;
+    } else {
+        location.href = `${window.origin}/review/?sortType=${$buttonForm['sortType'].value}&sort=${$buttonForm['sort'].checked}&keyword=${$searchInput.value}`;
+    }
+});
+
+let isClick = $searchInput.classList.contains('visible');
+$buttonForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (!isClick) {
+        $searchInput.classList.add('visible');
+        isClick = true;
+    } else {
+        $buttonForm.submit();
     }
 });
